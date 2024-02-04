0cd9b6d057020ed95f45c877cc4f4dd88e3429e7
RosenValev <121716535+RosenValev@users.noreply.github.com>
2024-02-04 13:15:07 +0200

    Smooth scrolling added

--- Build Responsive Real-World Websites with HTML and CSS/starter/09-Omnifood-Optimizations/script.js
+++ Build Responsive Real-World Websites with HTML and CSS/starter/09-Omnifood-Optimizations/script.js
@@ -1,7 +1,9 @@
 ///////////////////////////////////////////////////////////
 console.log("zdrrr")
-// Make mobile button work
 
+
+
+// Make mobile button work
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
@@ -12,10 +14,33 @@ btnNavEl.addEventListener("click", function () {
 
 
 
+// Smooth scrolling animation
+const allLins = document.querySelectorAll("a:link");
+allLins.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
 
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
 
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" })
+    }
 
-
+    //Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  })
+});
 
 
 
